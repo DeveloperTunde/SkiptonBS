bf4c87634ff24ae37d97b7aa2585ec6caaa4aef6
DeveloperTunde <85291994+DeveloperTunde@users.noreply.github.com>
2025-11-17 10:04:54 +0000

    fix: resolved Jest configuration for CommonJS compatibility

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
-module.exports = {
+// jest.config.js
+export default {
   preset: "jest-expo",
   transformIgnorePatterns: [
-    "node_modules/(?!((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@react-native-async-storage/async-storage))",
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg))",
   ],
   setupFilesAfterEnv: [
     "@testing-library/jest-native/extend-expect",
-    "./jest.setup.js",
+    "<rootDir>/jest.setup.js",
   ],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   testMatch: [
